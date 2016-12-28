6ebe264294096a3078dd50764eb93378afec46a8
jhoun <jayhoun@gmail.com>
2016-12-27 16:20:39 -1000

    created socket errors and server errors

--- server.js
+++ server.js
@@ -3,13 +3,28 @@ const net = require('net');
 
 //server.address()
 var server = net.createServer((socket) => {
+  //welcomes you to the server
   console.log('Welcome', socket);
 
-  // //if socket emits error
-  // socket.on('data', (data))
+  //shows that data is being sent
+  socket.on('data', (data) => {
+    console.log('you sent data!');
+  });
+
+  //this will appear if SOCKET emits error
+  client.on('error', (err) => {
+    throw err;
+  })
+
 });
 
+//this will appear if SERVER emits error
+server.on('error', (err) => {
+  throw err;
+})
 
+
+//starts your server
 server.listen(6969,() => {
   console.log('opened server on', server.address());
 });
